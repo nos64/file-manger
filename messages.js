7e43b4746390b8d1771df4990bc10e2fdf256eb7
Mikhail Nosov <manosov1984@gmail.com>
2025-05-02 13:34:45 +0300

    feat: implement error message module

---
+++ messages.js
@@ -0,0 +1,7 @@
+export const showInvalidInput = () => {
+  console.log('\x1b[31mInvalid input\x1b[0m');
+};
+
+export const showOperationFailed = () => {
+  console.log('\x1b[31m\x1b[1mOperation failed\x1b[0m');
+};
