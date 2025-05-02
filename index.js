1721f078d4cbae2bf5efbe8bc01c08ddabc50c13
Mikhail Nosov <manosov1984@gmail.com>
2025-05-02 13:06:18 +0300

    feat: add createInterface and display exit message.

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-class FileManager {
-  constructor(username) {
-    this.username = username;
+import { chdir } from 'node:process';
+import { homedir } from 'node:os';
+import { stdin as input, stdout as output } from 'node:process';
+import { createInterface } from 'node:readline/promises';
+
+import { showCurrentDirectory } from './navigation.js';
+
+const username = process.argv
+  .find(arg => arg.startsWith('--username='))
+  ?.split('=')[1]
+  ?? 'User';
+
+console.log(`Welcome to the File Manager, ${username}!`);
+chdir(homedir());
+showCurrentDirectory();
+
+const rl = createInterface({ input, output });
+
+rl.on('line', async (input) => {
+  try {
+    if (input === '.exit') {
+      rl.close();
+      return;
+    }
+
+  } catch (error) {
+    if (error.message === 'Invalid input') {
+
+    } else {
+    }
+  } finally {
+    showCurrentDirectory();
   }
+});
 
-  async start() {
-    console.log(`Welcome to the File Manager, ${this.username}!`);
-  }
-}
-
-const usernameFromArg = process.argv.find(arg => arg.startsWith('--username='));
-const username = usernameFromArg ? usernameFromArg.split('=')[1] : 'User';
-
-
-const fileManager = new FileManager(username);
-fileManager.start().catch(console.error);
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  process.exit(0);
+});
