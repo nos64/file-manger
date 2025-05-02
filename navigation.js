b778273154dde6ad3076a3b55f0d6f9f3a9ccc48
Mikhail Nosov <manosov1984@gmail.com>
2025-05-02 13:16:27 +0300

    feat: implement navigation functions

--- navigation.js
+++ navigation.js
@@ -1,5 +1,19 @@
-import { cwd } from 'node:process';
+import { chdir, cwd } from 'node:process';
+import { join } from 'node:path';
 
 export const showCurrentDirectory = () => {
-  console.log(`You are currently in \x1b[32m${cwd()}\x1b[0m`);
+  console.log(`You are currently in \x1b[32m${cwd()}\x1b[0m \n`);
+};
+
+export const navigateUp = () => {
+  const currentDir = cwd();
+  const parentDir = join(currentDir, '..');
+  
+  if (parentDir !== currentDir) {
+    chdir(parentDir);
+  }
+};
+
+export const navigateTo = (path) => {
+  chdir(path);
 };
