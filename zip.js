60706ea47955dfaead033bd229b949038da4ee80
Mikhail Nosov <manosov1984@gmail.com>
2025-05-04 18:44:53 +0300

    refactor: update zip.js

--- zip.js
+++ zip.js
@@ -2,6 +2,8 @@ import { createReadStream, createWriteStream } from 'fs';
 import { createBrotliCompress, createBrotliDecompress } from 'zlib';
 import { pipeline } from 'stream/promises';
 
+import { getColorText } from './messages.js';
+
 export const compressFile = async (pathToFile, pathToDestination) => {
   try {
     await pipeline(
@@ -10,7 +12,7 @@ export const compressFile = async (pathToFile, pathToDestination) => {
       createWriteStream(pathToDestination)
     );
 
-    console.log(`File compressed successfully to \x1b[33m${pathToDestination}\x1b[0m`);
+    console.log(`File compressed successfully to ${getColorText('yellow', pathToDestination)} \n`);
   } catch (error) {
     console.error('Error compressing file:', error.message);
   }
@@ -23,7 +25,7 @@ export const decompressFile = async (pathToFile, pathToDestination) => {
       createBrotliDecompress(),
       createWriteStream(pathToDestination)
     );
-    console.log(`File decompressed successfully to \x1b[33m${pathToDestination}\x1b[0m`);
+    console.log(`File decompressed successfully to ${getColorText('yellow', pathToDestination)} \n`);
 } catch (error) {
   console.error('Error decompressing file:', error.message);
 }
